bd45a35a1d535db032d49982c3f49eab5c16b4c7
kostyantynvernik <kostyantynvernik@gmail.com>
2025-07-04 23:35:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -653,12 +653,14 @@ ELS_test.forEach(el => attrObserver.observe(el, {attributes: true}));
 
 
 
+
 document.querySelectorAll('.mobshow span')[0].addEventListener("click", function () {
  
  
 document.querySelectorAll("#search-location")[0].click();
+document.querySelectorAll(".mobvip")[0].click();
 
-
+document.getElementsByClassName("mobvip")[0].scrollIntoView({ behavior: "smooth", block: "start"});
 
 
 });
